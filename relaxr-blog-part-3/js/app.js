ced59e8379e17cdc8fb94a203e455ef4d905ddfe
cynthiachen88 <chen.cynthia.work@gmail.com>
2015-09-19 15:53:21 +1000

    First commit

---
+++ relaxr-blog-part-3/js/app.js
@@ -0,0 +1,25 @@
+/*global document, $ */
+(function () {
+    'use strict';
+
+    $(document).ready(function () {
+        /* ----- Begin writing your JavaScript here  ------*/
+        
+        
+    /* 1. If user clicks on "Read More" button:
+        1.1 Text in the <p> tag is made visible by sliding down
+        1.2 When the <p> tag text is made visible, a "Read Less" link is also shown
+       2. If user clicks "Read Less" link, the <p> tag slides up and the content is no longer visible
+    */    
+        
+
+        
+        
+        
+
+        
+        
+        /* ----- Finish writing your JavaScript here  -----*/
+    });
+}());
+
